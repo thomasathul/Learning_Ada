f257f16bd88150882bb0af124094f0b3eca97b9e
Athul Thomas <thomasathul@gmail.com>
2023-10-22 17:02:30 -0400

    Add files via upload

---
+++ src/addition.adb
@@ -0,0 +1,14 @@
+with Ada.Text_IO;
+use Ada.Text_IO;
+with Ada.Integer_Text_IO; 
+use Ada.Integer_Text_IO;
+
+procedure addition is
+   A,B,C: Integer;
+begin
+   A:=10;
+   B:=20;
+   C:=A+B;
+   Put("Sum:");
+   Put(C);
+end addition;
